dea7205972c73cb760663f901746b56765efc1a1
AMIT-1988 <91830588+AMIT-1988@users.noreply.github.com>
2024-05-25 23:10:52 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ def est_authentifie():
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
+    return render_template('hello.html') #commit 2
 
 @app.route('/lecture')
 def lecture():
